ed479d3c34507277cec59c2db296e39d4cbc3117
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2024-05-30 00:51:27 +0200

    organize routes

--- flaskr/routes/user_routes.py
+++ flaskr/routes/user_routes.py
@@ -6,7 +6,6 @@ from controllers.user_controller import (
     get_user_by_id,
     delete_user,
 )
-from controllers.group_member_controller import add_user_to_group
 
 user_blueprint = Blueprint("user", __name__)
 
@@ -14,7 +13,4 @@ user_blueprint.route("/", methods=["GET"])(get_users)
 user_blueprint.route("/", methods=["POST"])(create_user)
 user_blueprint.route("/<int:user_id>", methods=["GET"])(get_user_by_id)
 user_blueprint.route("/<int:user_id>", methods=["DELETE"])(delete_user)
-user_blueprint.route("/<int:user_id>/<int:group_id>", methods=["POST"])(
-    add_user_to_group
-)
 user_blueprint.route("/<int:user_id>/groups", methods=["GET"])(get_groups_from_user)
