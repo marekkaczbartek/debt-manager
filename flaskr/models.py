462d30120f94e4050900fa28eecd548cb269b505
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2024-05-30 00:50:43 +0200

    Delete GroupMember model, replace it with many-to-many relationship handler

--- flaskr/models.py
+++ flaskr/models.py
@@ -1,12 +1,14 @@
 from config import db
 
 
-class User(db.Model):
-    # id: int
-    # username: str
-    # password: str
-    # email: str
+group_user = db.Table(
+    "group_user",
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id")),
+    db.Column("group_id", db.Integer, db.ForeignKey("group.id")),
+)
 
+
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
@@ -39,23 +41,26 @@ class Debt(db.Model):
         self.user_from_id = user_from_id
         self.user_to_id = user_to_id
 
+    def to_json(self):
+        return {
+            "id": self.id,
+            "amount": self.amount,
+            "description": self.description,
+            "date": self.date,
+            "group_id": self.group_id,
+            "user_from_id": self.user_from_id,
+            "user_to_id": self.user_to_id,
+            "settled": self.settled,
+        }
+
 
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
+    users = db.relationship("User", secondary=group_user, backref="groups")
 
     def __init__(self, name):
         self.name = name
 
     def to_json(self):
         return {"id": self.id, "name": self.name}
-
-
-class GroupMember(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    group_id = db.Column(db.Integer, db.ForeignKey("group.id"), nullable=False)
-
-    def __init__(self, user_id, group_id):
-        self.user_id = user_id
-        self.group_id = group_id
