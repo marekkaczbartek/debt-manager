953f51d2041dce5c6bc07b7dcc4bbce3cde9c67a
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2024-05-30 15:45:15 +0200

    frontend init

--- main/routes/debt_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from controllers import debt_controller
-
-debt_blueprint = Blueprint("debt", __name__)
-
-debt_blueprint.route("/", methods=["POST"])(debt_controller.add_debt_for_multiple_users)
-debt_blueprint.route("/", methods=["GET"])(debt_controller.get_debts)
